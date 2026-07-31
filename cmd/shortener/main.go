4dc65af3ba5ded8969413293443185a9bea05c63
MartsinovichDanya <123@123.ru>
2026-07-31 22:48:37 +0300

    remove env

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,18 +1,14 @@
 package main
 
 import (
-	"log"
-
-	"github.com/joho/godotenv"
-
 	"github.com/MartsinovichDanya/pgc_shortener/internal/server"
 )
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
+	//err := godotenv.Load()
+	//if err != nil {
+	//	log.Fatal("Error loading .env file")
+	//}
 
 	server.Run()
 }
